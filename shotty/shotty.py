c34107159bcdd76198f9167ca10ec99c8e30359f
ahmedsalahshihab <ahmed.salah.shihab@gmail.com>
2021-04-27 01:38:45 +0100

    Update shotty.py

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,9 +1,52 @@
 import boto3
+import click
+
+session = boto3.Session(profile_name='shotty')
+ec2sr = session.resource('ec2')
+
+def filter_instances(project):
+	filters = [{'Name': 'tag:Project', 'Values': [project]}]
+	if project:
+		return ec2sr.instances.filter(Filters=filters)
+	else:
+		return ec2sr.instances.all()
+
+####################################################################
+
+@click.group()
+def instances():
+	"Commands for EC2 instances"
+
+@instances.command('list')
+@click.option('--project', default=None, help='Only instances for project (tag Project:<name>)')
+def list_instances(project):
+	"List EC2 instances"
+
+	instances_list = filter_instances(project)
+	for i in instances_list:
+		print(', '.join([i.instance_id, i.instance_type, i.public_dns_name, i.placement['AvailabilityZone'], i.state['Name'], str(i.tags)]))
+
+@instances.command('start')
+@click.option('--project', default=None, help='Only instances for project (tag Project:<name>)')
+def start_instances(project):
+	"Starts EC2 instances"
+
+	instances_list = filter_instances(project)
+	for i in instances_list:
+			i.start()
+			print("Starting EC2 instance " + str(i.instance_id))
+
+@instances.command('stop')
+@click.option('--project', default=None, help='Only instances for project (tag Project:<name>)')
+def stop_instances(project):
+	"Stops EC2 instances"
+
+	instances_list = filter_instances(project)
+	for i in instances_list:
+			i.stop()
+			print("Stopping EC2 instance " + str(i.instance_id))
+
+####################################################################
 
 if __name__ == '__main__':
-
-	session = boto3.Session(profile_name='shotty')
-	ec2sr = session.resource('ec2')
-
-	for i in ec2sr.instances.all():
-		print(i)
+	instances() 
